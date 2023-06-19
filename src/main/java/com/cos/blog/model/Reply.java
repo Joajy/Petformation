3e99337ab8e2d41e97c3ca8c96b1ed5f19f8d0f9
Mr.Kim <joajy98@gmail.com>
2023-06-19 23:09:48 +0900

    chore: change data type of id(int -> Long)

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -18,7 +18,7 @@ public class Reply {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false, length = 200)
     private String content;
--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -19,7 +19,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false, length = 30)
     private String username;
@@ -29,7 +29,6 @@ public class User {
 
     @Column(nullable = false, length = 50)
     private String email;
-
 
     @ColumnDefault("user")
     @Enumerated(EnumType.STRING)
