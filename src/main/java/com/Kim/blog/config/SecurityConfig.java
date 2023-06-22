e4b91f3a0db6247447480f59503c0e5a55fa4b1e
Mr.Kim <joajy98@gmail.com>
2023-06-22 10:30:42 +0900

    refactor: change WebSecurityConfigurerAdapter to SecurityFilterChain (WebSecurityConfigurerAdapter is deprecated -> Register SecurityFilterChain Bean)

--- src/main/java/com/Kim/blog/config/SecurityConfig.java
+++ src/main/java/com/Kim/blog/config/SecurityConfig.java
@@ -1,19 +1,17 @@
 package com.Kim.blog.config;
 
 //import com.Kim.blog.config.auth.PrincipalDetailService;
-import com.Kim.blog.config.auth.PrincipalDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 //Bean 등록으로 스프링 컨테이너에서 객체 관리
-@Configuration //Bean 등록
+/*@Configuration //Bean 등록
 @EnableWebSecurity  //Add Security Filter
 @EnableGlobalMethodSecurity(prePostEnabled = true)  //특정 url 접근 시 권한 및 인증 확인
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -49,24 +47,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/");
 //                .failureUrl("/auth/loginForm");
     }
-}
+}*/
 
 //WebSecurityConfigurerAdapter가 deprecated인 상태임을 확인, 이후 코드를 작성해가며 오류 검증 후 문제 없을 시 최종장에서 해당 코드로 수정할 예정
 //하단의 코드는 최신 Spring Security 적용 결과
-/*
 @Configuration //Bean 등록
 @EnableWebSecurity  //Add Security Filter
 @EnableGlobalMethodSecurity(prePostEnabled = true)  //특정 url 접근 시 권한 및 인증 확인
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    public BCryptPasswordEncoder encodePWD(){
-        return new BCryptPasswordEncoder();
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
                 .permitAll()
@@ -75,23 +69,9 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/auth/loginForm")
-                .loginProcessingUrl("/auth/loginProc"); //Spring Security에서 이 주소로 오는 Login Request를 가로챈다
+                .loginProcessingUrl("/auth/loginProc") //Spring Security에서 이 주소로 오는 Login Request를 가로챈다
                 .defaultSuccessUrl("/")
+                .failureUrl("/auth/loginForm")
                 .and().build();
     }
-
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeRequests()
-//                .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//            .and()
-//                .formLogin()
-//                .loginPage("/auth/loginForm");
-//    }
 }
-*/
