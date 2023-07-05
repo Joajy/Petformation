9c5723aab1f0b9f586d60c930076a9e0583e0532
Mr.Kim <joajy98@gmail.com>
2023-07-05 11:34:34 +0900

    refactor: imigrate kakaoLogin(UserController -> UserService)

--- src/main/java/com/Kim/blog/controller/UserController.java
+++ src/main/java/com/Kim/blog/controller/UserController.java
@@ -1,27 +1,11 @@
 package com.Kim.blog.controller;
 
-import com.Kim.blog.model.KakaoProfile;
-import com.Kim.blog.model.OAuthToken;
-import com.Kim.blog.model.RoleType;
-import com.Kim.blog.model.User;
 import com.Kim.blog.service.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 
 /*
     인증이 필요한 사용자에게 /auth/** 출입 경로 허용
@@ -30,13 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class UserController {
 
-    //Absolutely important Main key(Never leaked!)
-    @Value("${BlueStar.key}")
-    private String blueStarKey;
-
     private final UserService userService;
-
-    private final AuthenticationManager authenticationManager;
 
     @GetMapping("/auth/joinForm")
     public String joinForm() {
@@ -59,95 +37,7 @@ public class UserController {
 
     @GetMapping("/auth/kakao/callback")
     public String kakaoCallback(String code) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        //Make new HttpHeader Object
-        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//        headers.add("Accept", "application/json");
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        //Make new HttpBody Object
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", "d8b0fe0ec31fa20c787dcf4706b50843");
-        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
-        params.add("code", code);
-
-        //HttpHeader & HttpBody를 하나의 Object에 보관
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest
-                = new HttpEntity<>(params, headers);
-
-        //Http Request by POST
-        //Receive Response
-        ResponseEntity<String> response = restTemplate.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
-//        String Request_URL = "https://kauth.kakao.com/oauth/token";
-//        ResponseEntity<String> response =
-//                restTemplate.postForEntity(Request_URL, kakaoTokenRequest, String.class);
-
-        //Save Token Info to Object
-        ObjectMapper objectMapper = new ObjectMapper();
-        OAuthToken oAuthToken = null;
-        try {
-            oAuthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-        } catch(JsonMappingException e){
-            e.printStackTrace();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Kakao Access Token: "+ oAuthToken.getAccess_token());
-
-        //Access Token for request user profile
-        RestTemplate profileRestTemplate = new RestTemplate();
-
-        HttpHeaders profileHeaders = new HttpHeaders();
-        profileHeaders.add("Authorization", "Bearer " + oAuthToken.getAccess_token());
-        profileHeaders.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest
-                = new HttpEntity<>(profileHeaders);
-
-        ResponseEntity<String> profileResponse = profileRestTemplate.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoProfileRequest,
-                String.class
-        );
-
-        ObjectMapper profileObjectMapper = new ObjectMapper();
-        KakaoProfile kakaoProfile = null;
-        try {
-            kakaoProfile = profileObjectMapper.readValue(profileResponse.getBody(), KakaoProfile.class);
-        } catch(JsonMappingException e){
-            e.printStackTrace();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        User kakaoUser = User.builder()
-                .username(kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId())
-                .password(blueStarKey)
-                .email(kakaoProfile.getKakao_account().getEmail())
-                .role(RoleType.USER)
-                .oauth("kakao")
-                .build();
-
-        //check if that id was already joined
-        User originUser = userService.findUser(kakaoUser.getUsername());
-
-        if(originUser.getUsername() == null) {
-            userService.save(kakaoUser);
-        }
-
-        //processing Login
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(kakaoUser.getUsername(), blueStarKey));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
+        userService.kakaoLogin(code);
         return "redirect:/";
     }
 }
--- src/main/java/com/Kim/blog/service/UserService.java
+++ src/main/java/com/Kim/blog/service/UserService.java
@@ -1,16 +1,31 @@
 package com.Kim.blog.service;
 
 import com.Kim.blog.dto.UserRequestDto;
+import com.Kim.blog.model.KakaoProfile;
+import com.Kim.blog.model.OAuthToken;
 import com.Kim.blog.model.RoleType;
 import com.Kim.blog.model.User;
 import com.Kim.blog.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +35,11 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+    private final AuthenticationManager authenticationManager;
+
+    //Absolutely important Main key(Never leaked!)
+    @Value("${BlueStar.key}")
+    private String blueStarKey;
 
     @Transactional
     public void save(User user) {
@@ -43,12 +63,6 @@ public class UserService {
         userRepository.save(user);
     }
 
-    @Transactional(readOnly = true)
-    public User findUser(String username) {
-        User user = userRepository.findByUsername(username).orElseGet(User::new);
-        return user;
-    }
-
     @Transactional
     public void update(UserRequestDto userDto) {
         User persistence = userRepository.findByUsername(userDto.getUsername()).orElseThrow(() -> new IllegalArgumentException("해당하는 회원정보가 없습니다."));
@@ -64,21 +78,6 @@ public class UserService {
         }
     }
 
-//    @Transactional
-//    public void update(User user) {
-//        User persistence = userRepository.findById(user.getId()).orElseThrow(() -> new IllegalArgumentException("해당하는 회원정보가 없습니다."));
-//
-//        //Check Validation
-//        //if oAuth field has no value, can change email & password of that account
-//        String oAuth = persistence.getOauth();
-//        if(oAuth == null || oAuth.equals("")) {
-//            String rawPassword = user.getPassword();
-//            String encPassword = encoder.encode(rawPassword);
-//            persistence.setPassword(encPassword);
-//            persistence.setEmail(user.getEmail());
-//        }
-//    }
-
     @Transactional(readOnly = true)
     public Map<String, String> validateHandling(BindingResult bindingResult) {
         Map<String, String> validatorResult = new HashMap<>();
@@ -88,5 +87,82 @@ public class UserService {
             validatorResult.put(format, error.getDefaultMessage());
         }
         return validatorResult;
+    }
+
+    @Transactional
+    public void kakaoLogin(String code){
+        RestTemplate restTemplate = new RestTemplate();
+        //Make new HttpHeader Object
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        //Make new HttpBody Object
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "d8b0fe0ec31fa20c787dcf4706b50843");
+        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+        params.add("code", code);
+
+        //HttpHeader & HttpBody를 하나의 Object에 보관
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest
+                = new HttpEntity<>(params, headers);
+
+        //Http Request by POST
+        //Receive Response
+        String Request_URL = "https://kauth.kakao.com/oauth/token";
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(Request_URL, kakaoTokenRequest, String.class);
+
+        //Save Token Info to Object
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oAuthToken = null;
+        try {
+            oAuthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch(JsonMappingException e){
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        //Access Token for request user profile
+        RestTemplate profileRestTemplate = new RestTemplate();
+        HttpHeaders profileHeaders = new HttpHeaders();
+        profileHeaders.add("Authorization", "Bearer " + oAuthToken.getAccess_token());
+        profileHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest
+                = new HttpEntity<>(profileHeaders);
+
+        String profileRequest_URL = "https://kapi.kakao.com/v2/user/me";
+        ResponseEntity<String> profileResponse =
+                profileRestTemplate.postForEntity(profileRequest_URL, kakaoProfileRequest, String.class);
+
+        ObjectMapper profileObjectMapper = new ObjectMapper();
+        KakaoProfile kakaoProfile = null;
+        try {
+            kakaoProfile = profileObjectMapper.readValue(profileResponse.getBody(), KakaoProfile.class);
+        } catch(JsonMappingException e){
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        User kakaoUser = User.builder()
+                .username(kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId())
+                .password(blueStarKey)
+                .email(kakaoProfile.getKakao_account().getEmail())
+                .role(RoleType.USER)
+                .oauth("kakao")
+                .build();
+
+        //check if that id was already joined
+        User originUser = userRepository.findByUsername(kakaoUser.getUsername()).orElseGet(User::new);
+
+        if(originUser.getUsername() == null) save(kakaoUser);
+
+        //processing Login
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(kakaoUser.getUsername(), blueStarKey));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
