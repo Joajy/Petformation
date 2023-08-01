b4c5c64832e44228770070925771b829b7b0585a
Mr.Kim <joajy98@gmail.com>
2023-08-01 11:24:23 +0900

    refactor: delete unused method in repository package

--- src/main/java/com/Kim/blog/repository/BoardRepository.java
+++ src/main/java/com/Kim/blog/repository/BoardRepository.java
@@ -1,23 +1,18 @@
 package com.Kim.blog.repository;
 
 import com.Kim.blog.model.Board;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, JpaSpecificationExecutor<Board> {
 
     @Modifying
     @Query("update Board b set b.count = b.count + 1 where b.id = :id")
     void updateHit(Long id);
-
-    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
-
-    Page<Board> findByUserNicknameContaining(String searchKeyword, Pageable pageable);
 
     //LAG로 이전 행의 값 리턴
     @Query(value = "SELECT * FROM board "
--- src/main/java/com/Kim/blog/repository/UserRepository.java
+++ src/main/java/com/Kim/blog/repository/UserRepository.java
@@ -2,14 +2,13 @@ package com.Kim.blog.repository;
 
 import com.Kim.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-//    User findByUsernameAndPassword(String username, String password);
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUsername(String username);
 
